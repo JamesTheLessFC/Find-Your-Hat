e230f891245d322cbb6f3a13e8b0e070b64257b2
JamesTheLessFC <68306835+JamesTheLessFC@users.noreply.github.com>
2020-08-15 01:51:10 -0400

    added color graphics and field validator

---
+++ README.md
@@ -0,0 +1,24 @@
+#FIND YOUR HAT
+
+In this Codecademy project, I built an interactive terminal game. The scenario is that the player has lost their hat in a field full of holes, and they must navigate back to it without falling down one of the holes or stepping outside of the field.
+
+Required Features:
+
+When a user runs main.js, they should be prompted for input and be able to indicate which direction they’d like to “move”.
+After entering an instruction, the user should see a printed result of their current field map with the tiles they have visited marked with an asterisk. They should be prompted for their next move.
+This should continue until the user either:
+
+-Wins by finding their hat.
+-Loses by landing on (and falling in) a hole.
+-Attempts to move “outside” the field.
+
+When any of the above occur, the user is notified and the game ends.
+
+ADDED Features:
+
+-Character starts in a random location instead of the upper left
+-'Hard mode' where a new hole is added after every move
+-color graphics
+-field validator to ensure that the generated field is solvable
+-scoreboard
+-upon game completion, player is asked if they want to play again.
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const prompt = require('prompt-sync')({sigint: true});
+const term = require( 'terminal-kit' ).terminal ;
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+let playerScore = 0;
+let computerScore = 0;
 
 class Field {
   constructor(field) {
@@ -12,7 +15,7 @@ class Field {
     this.playerLocationY;
     this.mode;
   }
-
+  //determine player starting location
   setPlayerLocation() {
     this.field.forEach(line => {
       if (line.includes(pathCharacter)) {
@@ -21,15 +24,18 @@ class Field {
       }
     });
   }
-
+  //print field
   print() {
     this.field.forEach(line => {
+      term.black();
+      term.bgBrightCyan();
       console.log(line.join(''));
+      term.styleReset();
     });
   }
-
+  //prompt player for their next move
   getDirection() {
-    let direction = prompt('Which direction? u/d/r/l');
+    let direction = prompt('Which direction? [u/d/r/l]');
     switch (direction) {
       case 'u':
         this.playerLocationY -= 1;
@@ -44,30 +50,53 @@ class Field {
         this.playerLocationX += 1;
         break;
       default:
+        term.red();
         console.log('Invalid entry');
+        term.defaultColor();
     }
   }
 
   testForGameEnd() {
-    const currentLocation = this.field[this.playerLocationY][this.playerLocationX];
-    if (!currentLocation || currentLocation === hole) {
-      console.log('Computer wins!');
+    //get current location of player
+    const currentLocation = this.playerLocationY !== this.field.length && this.playerLocationY >= 0 && this.playerLocationX !== this.field[0].length && this.playerLocationX >= 0 ? this.field[this.playerLocationY][this.playerLocationX] : undefined;
+    //determine if game is over; if so, update and score and notify player
+    if (currentLocation === hole) {
+      computerScore += 1;
+      term.yellow();
+      console.log('You fell into a hole. Computer wins!');
+      console.log(`Score: Computer - ${computerScore}, You - ${playerScore}`);
+      term.defaultColor();
+      this.playAgain();
+    } else if (currentLocation === undefined) {
+      computerScore += 1;
+      term.yellow();
+      console.log('You fell off a cliff. Computer wins!');
+      console.log(`Score: Computer - ${computerScore}, You - ${playerScore}`);
+      term.defaultColor();
       this.playAgain();
     } else if (currentLocation === hat) {
-      console.log('You win!');
+      playerScore += 1;
+      term.green();
+      console.log('Hat found. You win!');
+      console.log(`Score: Computer - ${computerScore}, You - ${playerScore}`);
+      term.defaultColor();
       this.playAgain();
     } else {
+      //game isn't over so update field
       this.field[this.playerLocationY].splice(this.playerLocationX, 1, pathCharacter);
+      //add hole if the game is set to hard mode
       if (this.mode === 'hard') {
         this.addHoles();
       }
+      //start next turn
       this.playGame();
     }
   }
 
-  playGame() {
+  async playGame() {
+    //if new game, ask player for their preferred mode
     if (this.mode === undefined) {
-      let answer = prompt('easy or hard mode? e/h');
+      let answer = prompt('easy or hard mode? [e/h]');
       switch (answer) {
         case 'e':
           this.mode = 'easy';
@@ -76,38 +105,50 @@ class Field {
           this.mode = 'hard';
           break;
         default:
+          term.red();
           console.log('Invalid entry');
+          term.defaultColor();
           this.playGame();
       }
     }
+    //if new game, get player starting location
     if (this.playerLocationX === undefined) {
       this.setPlayerLocation();
     }
+    //print field
     this.print();
+    //prompt player for their next move
     this.getDirection();
+    //check for game resolution
     this.testForGameEnd();
   }
 
   playAgain() {
-    let answer = prompt('Play again? y/n');
+    //ask player if they would like to play again; if so, start a new game
+    let answer = prompt('Play again? [y/n]');
     switch (answer) {
       case 'y':
         const newField = new Field(Field.generateField(10, 10, 30));
         newField.playGame();
         break;
       case 'n':
-        console.log('OK');
+        term.yellow();
+        console.log('OK. Goodbye!');
+        term.defaultColor();
         break;
       default:
+        term.red();
         console.log('Invalid entry');
+        term.defaultColor();
         this.playAgain();
     }
   }
 
   addHoles() {
+    //get path location
     const pathRowNum = this.playerLocationY;
     const pathColumnNum = this.playerLocationX;
-
+    //get hat location
     let hatRowNum;
     let hatColumnNum;
     this.field.forEach(line => {
@@ -116,17 +157,73 @@ class Field {
         hatRowNum = this.field.indexOf(line);
       }
     });
-
+    //determine random location for new hole
     let holeRowNum = Math.floor(Math.random() * this.field.length);
     let holeColumnNum = Math.floor(Math.random() * this.field[0].length);
-
+    //make sure hole location is not a path or hat; if so, get new location
     while ((holeRowNum === hatRowNum && holeColumnNum === hatColumnNum) || (holeRowNum === pathRowNum && holeColumnNum === pathColumnNum)) {
       holeRowNum = Math.floor(Math.random() * this.field.length);
-      holeColunmNum = Math.floor(Math.random() * this.field[0].length);
+      holeColumnNum = Math.floor(Math.random() * this.field[0].length);
     }
-
+    //add new hole to field
     this.field[holeRowNum].splice(holeColumnNum, 1, hole);
   }
+
+  isValidField() {
+    let field = this.field;
+    //set up field to mark 'visited' locations
+    let visited = [];
+    for (let row = 0; row < field.length; row++) {
+      let newLine = [];
+      for (let col = 0; col < field[0].length; col++) {
+        newLine.push(false);
+      }
+      visited.push(newLine);
+    }
+    //determine player starting location
+    this.setPlayerLocation();
+    let x = this.playerLocationX;
+    let y = this.playerLocationY;
+    const xMax = field[0].length - 1;
+    const yMax = field.length - 1;
+    // determine if location is a hat or hole - return true for hat, false for hole
+    const findPath = (x, y) => {
+      if (field[y][x] === hat) {
+        return true;
+      }
+      if (field[y][x] === hole || visited[y][x]) {
+        return false;
+      }
+      //mark location as visited
+      visited[y][x] = true;
+      //check nearby paths, continue looping findPath function until all paths are exhausted
+      if (y !== 0) {
+        if (findPath(x, y-1)) {
+          return true;
+        }
+      }
+      if (y !== yMax) {
+        if (findPath(x, y+1)) {
+          return true;
+        }
+      }
+      if (x !== 0) {
+        if (findPath(x-1, y)) {
+          return true;
+        }
+      }
+      if (x !== xMax) {
+        if (findPath(x+1, y)) {
+          return true;
+        }
+      }
+      //if no paths are found, return false
+      return false;
+    }
+    //start findPath function loop here
+    return findPath(x, y);
+  }
+
 
   static generateField(height, width, holePercentage) {
     const field = [];
@@ -162,6 +259,11 @@ class Field {
 
 }
 
-const myField = new Field(Field.generateField(10, 10, 30));
-
+//set up new field
+let myField = new Field(Field.generateField(10, 10, 30))
+//make sure field is solvable
+while (!myField.isValidField()) {
+  let myField = new Field(Field.generateField(10, 10, 30));
+}
+//start game
 myField.playGame();
